6dbcde759338221efbcecb0a36620b02f5a6c273
Mark Rada <markrada26@gmail.com>
2022-08-29 11:32:05 -0400

    Properly skip over app_group_id headers (#8)
    Array length is written ahead of the array, so if we want
    to skip over the app_group_id headers, then we need to filter
    out the app_group_id header before writing the array length.

--- spec/record_spec.rb
+++ spec/record_spec.rb
@@ -8,11 +8,19 @@ describe Kafka::Protocol::Record do
     record = described_class.new(value: 'test', headers: { app_group_id: 'blueberry' })
     record.encode(encoder)
     expect(encoded_io.string['blueberry']).to be_nil
+
+    decoder = Kafka::Protocol::Decoder.from_string(encoded_io.string)
+    decoded_record = Kafka::Protocol::Record.decode(decoder)
+    expect(decoded_record.headers).to be_empty
   end
 
   it 'does encode other headers' do
     record = described_class.new(value: 'test', headers: { fruit: 'raspberry' })
     record.encode(encoder)
     expect(encoded_io.string['raspberry']).to_not be_nil
+
+    decoder = Kafka::Protocol::Decoder.from_string(encoded_io.string)
+    decoded_record = Kafka::Protocol::Record.decode(decoder)
+    expect(decoded_record.headers).to eq({ "fruit" => "raspberry" })
   end
 end
